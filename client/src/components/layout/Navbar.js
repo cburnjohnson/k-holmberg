47a965e05480c0661d7602df13c16b8c133ff8e6
Cole Johnson <cburnjohnson@gmail.com>
2020-04-28 13:41:25 -0700

    navbar styling

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
         <div className='nav'>
-            <div className='nav-brand'>K.Holmberg</div>
+            <div className='nav-brand'>
+                <Link to='/'>K.Holmberg</Link>
+            </div>
             <div className='nav-links'>
                 <ul>
-                    <li>Gallery</li>
+                    <li>
+                        <Link to='/'>Gallery</Link>
+                    </li>
                 </ul>
             </div>
         </div>
