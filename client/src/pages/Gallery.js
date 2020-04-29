0ba69eb3f846ef89e104988a00e1530b4e9742b7
Cole Johnson <cburnjohnson@gmail.com>
2020-04-29 14:04:04 -0700

    css for gallery header

--- client/src/pages/Gallery.js
+++ client/src/pages/Gallery.js
@@ -5,11 +5,18 @@ import Pictures from '../components/gallery/Pictures';
 
 const Gallery = () => {
     return (
-        <>
-            <h1>G A L L E R Y</h1>
+        <div className='gallery'>
+            <h1>
+                <span className='blue'>G</span>{' '}
+                <span className='purple'>A</span>{' '}
+                <span className='green'>L</span>{' '}
+                <span className='green'>L</span> <span className='red'>E</span>{' '}
+                <span className='orange'>R</span>{' '}
+                <span className='yellow'>Y</span>
+            </h1>
             <Pictures />
             <BackgroundImages />
-        </>
+        </div>
     );
 };
 
