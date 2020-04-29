87065d0223db578aa7e48572b249c4c255eda812
Cole Johnson <cburnjohnson@gmail.com>
2020-04-29 13:41:24 -0700

    mobile images

--- client/src/components/home/BackgroundImages.js
+++ client/src/components/home/BackgroundImages.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import GreenPaint from '../../images/green-paint.png';
+import YellowStroke from '../../images/yellow-mobile.png';
+import PurpleStroke from '../../images/purple-mobile.png';
+import GreenStroke from '../../images/green-mobile.png';
+import RedStroke from '../../images/red-mobile.png';
+import BlueStroke from '../../images/blue-mobile.png';
 
 const BackgroundImages = () => {
     return (
         <div className='bg-images'>
-            <img src={GreenPaint} alt='' className='green-paint' />
+            <img src={PurpleStroke} alt='' className='paint-bg purple' />
+            <img src={BlueStroke} alt='' className='paint-bg blue' />
+            <img src={GreenStroke} alt='' className='paint-bg green' />
+            <img src={YellowStroke} alt='' className='paint-bg yellow' />
+            <img src={RedStroke} alt='' className='paint-bg red' />
         </div>
     );
 };
