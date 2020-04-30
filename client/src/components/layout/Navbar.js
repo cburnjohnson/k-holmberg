fe6337211733abace27d0fa5ff5d6fc23a453a80
Cole Johnson <cburnjohnson@gmail.com>
2020-04-30 13:48:03 -0700

    update nav links

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+    console.log(window.location.pathname);
+
+    const [pathname, setPathname] = useState(window.location.pathname);
+
+    const homeLinks = (
+        <>
+            <Link to='/gallery' onClick={() => setPathname('/gallery')}>
+                Gallery
+            </Link>
+        </>
+    );
+
+    const galleryLinks = (
+        <>
+            <button>Cart</button>
+        </>
+    );
+
     return (
         <nav className='nav'>
             <div className='nav-brand'>
-                <Link to='/'>K.Holmberg</Link>
+                <Link to='/' onClick={() => setPathname('/')}>
+                    K.Holmberg
+                </Link>
             </div>
             <div className='nav-links'>
                 <ul>
                     <li>
-                        <Link to='/gallery'>Gallery</Link>
+                        {pathname === '/gallery' ? galleryLinks : homeLinks}
                     </li>
                 </ul>
             </div>
