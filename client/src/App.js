37f4a1591295db0f3c97673e0d78a1ae7567c295
Cole Johnson <cburnjohnson@gmail.com>
2020-05-02 14:22:07 -0700

    bug fix

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import PictureState from './context/picture/PictureState';
+import CartState from './context/cart/CartState';
 
 import Navbar from './components/layout/Navbar';
 import Home from './pages/Home';
@@ -10,15 +11,17 @@ import Gallery from './pages/Gallery';
 function App() {
     return (
         <PictureState>
-            <Router>
-                <div className='container'>
-                    <Navbar />
-                    <Switch>
-                        <Route exact path='/' component={Home} />
-                        <Route exact path='/gallery' component={Gallery} />
-                    </Switch>
-                </div>
-            </Router>
+            <CartState>
+                <Router>
+                    <div className='container'>
+                        <Navbar />
+                        <Switch>
+                            <Route exact path='/' component={Home} />
+                            <Route exact path='/gallery' component={Gallery} />
+                        </Switch>
+                    </div>
+                </Router>
+            </CartState>
         </PictureState>
     );
 }
--- client/src/context/cart/CartState.js
+++ client/src/context/cart/CartState.js
@@ -2,6 +2,8 @@ import React, { useReducer } from 'react';
 import CartContext from './cartContext';
 import cartReducer from './cartReducer';
 
+import TestPicture from '../../images/test-picture.jpg';
+
 const CartState = (props) => {
     const initialState = {
         cartItems: [{ id: 1, src: TestPicture, alt: 'test', price: '$10' }],
