af88095ec9e4db928981dcc81afd2d303c186dfc
Cole Johnson <cburnjohnson@gmail.com>
2020-05-02 14:20:10 -0700

    initial cart context

---
+++ client/src/context/cart/CartState.js
@@ -0,0 +1,19 @@
+import React, { useReducer } from 'react';
+import CartContext from './cartContext';
+import cartReducer from './cartReducer';
+
+const CartState = (props) => {
+    const initialState = {
+        cartItems: [{ id: 1, src: TestPicture, alt: 'test', price: '$10' }],
+    };
+
+    const [state, dispatch] = useReducer(cartReducer, initialState);
+
+    return (
+        <CartContext.Provider value={{ cartItems: state.cartItems }}>
+            {props.children}
+        </CartContext.Provider>
+    );
+};
+
+export default CartState;
