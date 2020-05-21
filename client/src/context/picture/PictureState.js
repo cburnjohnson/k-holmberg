59ab8bc87576a69922a91711d875984f0c3dcc46
Cole Johnson <cburnjohnson@gmail.com>
2020-05-21 14:26:43 -0700

    added painting

--- client/src/context/picture/PictureState.js
+++ client/src/context/picture/PictureState.js
@@ -3,6 +3,7 @@ import PictureContext from './pictureContext';
 import pictureReducer from './pictureReducer';
 
 import TestPicture from '../../images/test-picture.jpg';
+import RiverForrest from '../../images/paintings/river-forrest.png';
 
 const PictureState = (props) => {
     const initialState = {
@@ -24,6 +25,13 @@ const PictureState = (props) => {
             {
                 id: 3,
                 src: TestPicture,
+                alt: 'test',
+                dimensions: '10x8',
+                price: 30,
+            },
+            {
+                id: 4,
+                src: RiverForrest,
                 alt: 'test',
                 dimensions: '10x8',
                 price: 30,
