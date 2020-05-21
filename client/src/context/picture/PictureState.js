0b87543182e27d69e84416f3d7352331686602b5
Cole Johnson <cburnjohnson@gmail.com>
2020-05-21 14:41:18 -0700

    added dancer painting

--- client/src/context/picture/PictureState.js
+++ client/src/context/picture/PictureState.js
@@ -2,37 +2,23 @@ import React, { useReducer } from 'react';
 import PictureContext from './pictureContext';
 import pictureReducer from './pictureReducer';
 
-import TestPicture from '../../images/test-picture.jpg';
 import RiverForrest from '../../images/paintings/river-forrest.png';
+import Dancer from '../../images/paintings/dancer.jpg';
 
 const PictureState = (props) => {
     const initialState = {
         pictures: [
             {
                 id: 1,
-                src: TestPicture,
-                alt: 'test',
-                dimensions: '10x8',
-                price: 10,
-            },
-            {
-                id: 2,
-                src: TestPicture,
-                alt: 'test',
-                dimensions: '10x8',
-                price: 10,
-            },
-            {
-                id: 3,
-                src: TestPicture,
-                alt: 'test',
+                src: RiverForrest,
+                alt: 'River forrest',
                 dimensions: '10x8',
                 price: 30,
             },
             {
-                id: 4,
-                src: RiverForrest,
-                alt: 'test',
+                id: 2,
+                src: Dancer,
+                alt: 'Dancer',
                 dimensions: '10x8',
                 price: 30,
             },
