35e1196c5e243672c5149b01d201d239b7f7d93e
Cole Johnson <cburnjohnson@gmail.com>
2020-05-26 13:40:32 -0700

    renaming

--- client/src/components/layout/BackgroundImages.js
+++ client/src/components/layout/BackgroundImages.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import YellowStroke from '../../images/yellow-mobile.png';
-import PurpleStroke from '../../images/purple-mobile.png';
-import GreenStroke from '../../images/green-mobile.png';
-import RedStroke from '../../images/red-mobile.png';
-import BlueStroke from '../../images/blue-mobile.png';
+import YellowStrokeM from '../../images/background/yellow-mobile.png';
+import PurpleStrokeM from '../../images/background/purple-mobile.png';
+import GreenStrokeM from '../../images/background/green-mobile.png';
+import RedStrokeM from '../../images/background/red-mobile.png';
+import BlueStrokeM from '../../images/background/blue-mobile.png';
 
 const BackgroundImages = () => {
     return (
         <div className='bg-images'>
-            <img src={PurpleStroke} alt='' className='paint-bg purple' />
-            <img src={BlueStroke} alt='' className='paint-bg blue' />
-            <img src={GreenStroke} alt='' className='paint-bg green' />
-            <img src={YellowStroke} alt='' className='paint-bg yellow' />
-            <img src={RedStroke} alt='' className='paint-bg red' />
+            <img src={PurpleStrokeM} alt='' className='paint-bg-m purple-m' />
+            <img src={BlueStrokeM} alt='' className='paint-bg-m blue-m' />
+            <img src={GreenStrokeM} alt='' className='paint-bg-m green-m' />
+            <img src={YellowStrokeM} alt='' className='paint-bg-m yellow-m' />
+            <img src={RedStrokeM} alt='' className='paint-bg-m red-m' />
         </div>
     );
 };
