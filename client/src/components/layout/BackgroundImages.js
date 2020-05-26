34f2513524d4f89d43bdf15e3221dc11604a1fb5
Cole Johnson <cburnjohnson@gmail.com>
2020-05-26 13:57:27 -0700

    desktop paint

--- client/src/components/layout/BackgroundImages.js
+++ client/src/components/layout/BackgroundImages.js
@@ -5,6 +5,12 @@ import GreenStrokeM from '../../images/background/green-mobile.png';
 import RedStrokeM from '../../images/background/red-mobile.png';
 import BlueStrokeM from '../../images/background/blue-mobile.png';
 
+import PurpleStrokeD from '../../images/background/purple-desktop.png';
+import YellowStrokeD from '../../images/background/yellow-desktop.png';
+import BlueStrokeD from '../../images/background/blue-desktop.png';
+import GreenStrokeD from '../../images/background/green-desktop.png';
+import OrangeStrokeD from '../../images/background/orange-desktop.png';
+
 const BackgroundImages = () => {
     return (
         <div className='bg-images'>
@@ -13,6 +19,12 @@ const BackgroundImages = () => {
             <img src={GreenStrokeM} alt='' className='paint-bg-m green-m' />
             <img src={YellowStrokeM} alt='' className='paint-bg-m yellow-m' />
             <img src={RedStrokeM} alt='' className='paint-bg-m red-m' />
+
+            <img src={PurpleStrokeD} alt='' className='paint-bg-d purple-d' />
+            <img src={YellowStrokeD} alt='' className='paint-bg-d yellow-d' />
+            <img src={BlueStrokeD} alt='' className='paint-bg-d blue-d' />
+            <img src={GreenStrokeD} alt='' className='paint-bg-d green-d' />
+            <img src={OrangeStrokeD} alt='' className='paint-bg-d orange-d' />
         </div>
     );
 };
